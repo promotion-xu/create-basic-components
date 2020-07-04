f9cfc1ca8371b8be3f0a0531f7679e6f14a3b5b6
xuzhen <>
2020-07-04 16:20:00 +0800

    fix: move readme

---
+++ scripts/CreateComponentsCLI/src/views/xuzhen-s/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './index.less'
+
+const XuzhenS = props => {
+  return <div>XuzhenS</div>
+}
+
+export default XuzhenS
---
+++ scripts/CreateComponentsCLI/src/views/xuzhen-s/models/index.ts
@@ -0,0 +1,12 @@
+import { withMixin } from "../../../helpers/dva";
+
+const init = {};
+export default withMixin({
+  namespace: "XuzhenS",
+  state: {
+    ...init
+  },
+  subscriptions: {},
+  effects: {},
+  reducers: {}
+});
